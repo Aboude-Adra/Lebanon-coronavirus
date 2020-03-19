62e92fbf4ebab801bffaa54ea682c2ed1582ef7a
Aboude <51809151+Aboude-Adra@users.noreply.github.com>
2020-03-19 20:39:15 +0200

    Changed data source & added number of daily cases

--- popup/index.js
+++ popup/index.js
@@ -1,16 +1,21 @@
 const fetchFromApi = async () => {
     return new Promise(async (res, rej) => {
-        const response = await fetch("https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=LB");
+        const response = await fetch("https://corona.lmao.ninja/countries/Lebanon");
         const json = await response.json();
 
-        const confirmed = json.locations[0].latest.confirmed
-        const deaths = json.locations[0].latest.deaths
-        const recovered = json.locations[0].latest.recovered
+        // const confirmed = json.locations[0].latest.confirmed
+        // const deaths = json.locations[0].latest.deaths
+        // const recovered = json.locations[0].latest.recovered
+        const confirmed = json.cases
+        const todayCases = json.todayCases
+        const deaths = json.deaths
+        const recovered = json.recovered
         
         res({
             "confirmed": confirmed,
             "deaths": deaths,
             "recovered": recovered,
+            "todayCases": todayCases,
         });
     });
 };
@@ -19,10 +24,12 @@ const setValues = (data) => {
     const totalEl = document.querySelector("#all-cases");
     const deathsEL = document.querySelector("#deaths");
     const recoveredEl = document.querySelector("#recovered");
+    const todayCasesEl = document.querySelector("#todayCases");
 
     totalEl.innerHTML = data.confirmed;
     deathsEL.innerHTML = data.deaths;
     recoveredEl.innerHTML = data.recovered;
+    todayCasesEl.innerHTML = data.todayCases;
 };
 
 window.onload = () => {
